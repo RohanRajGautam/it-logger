c49d7b15720173c2b51560886f8f440860501b9c
rohan <rohanrajgautam@gmail.com>
2020-07-16 18:20:46 +0545

    react, json-server, and materialize UI setup

--- src/App.js
+++ src/App.js
@@ -1,25 +1,22 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect } from "react";
+import "materialize-css/dist/css/materialize.min.css";
+import M from "materialize-css/dist/js/materialize.min.js";
+import "./App.css";
+import SearchBar from "./components/layout/SearchBar";
+import Logs from "./components/logs/Logs";
 
 function App() {
+  useEffect(() => {
+    M.AutoInit();
+  });
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <SearchBar />
+      <div className='container'>
+        <Logs />
+      </div>
+    </>
   );
 }
 
---
+++ src/components/logs/Logs.js
@@ -0,0 +1,38 @@
+import React, { useState, useEffect } from "react";
+
+const Logs = () => {
+  const [logs, setLogs] = useState([]);
+  const [loading, setLoading] = useState(false);
+
+  useEffect(() => {
+    getLogs();
+    //eslint-disable-next-line
+  }, []);
+
+  const getLogs = async () => {
+    setLoading(true);
+    const res = await fetch("/logs");
+    const data = await res.json();
+    setLogs(data);
+    setLoading(false);
+  };
+
+  if (loading) {
+    return <h4>Loading...</h4>;
+  }
+
+  return (
+    <ul className='collection-with-header'>
+      <li className='collection-header'>
+        <h4 className='center'>System Logs</h4>
+      </li>
+      {!loading && logs.length === 0 ? (
+        <h4>No Logs to show...</h4>
+      ) : (
+        logs.map((log) => <li>{log.message}</li>)
+      )}
+    </ul>
+  );
+};
+
+export default Logs;
